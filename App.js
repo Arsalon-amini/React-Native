8fe6a6f4b0512813bd65590bdb9e89bd292aa96b
arsalon <arsalonamini2020@gmail.com>
2020-10-27 17:29:12 +0800

    flexDirection, justifyContent, alignItems for layout

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: '#fff', flex: 1 }}> 
+    <View style={{ backgroundColor: '#fff',  flex: 1,
+     flexDirection: "row", //horizontal
+     justifyContent: "center", // main 
+     alignItems: "center"
+     }}> 
 
-    <View style={{backgroundColor: 'dodgerblue', flex: 1}}/>
-    <View style={{backgroundColor: 'gold', flex: 1}}/>
-    <View style={{backgroundColor: 'tomato', flex: 1}}/>
+    <View style={{backgroundColor: 'dodgerblue', flex: .5, }}/>
+    <View style={{backgroundColor: 'gold', flex: .5 }}/>
+    <View style={{backgroundColor: 'tomato', flex: .5 }}/>
 
     </View>
   );
