e67b6cc77ba0767828566cc3dfb521ab3d50e2a4
arsalon <arsalonamini2020@gmail.com>
2020-10-27 16:52:49 +0800

    detecting orientation using hooks

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, Alert, Button, Platform, StatusBar } from "react-native";
+import { 
+  StyleSheet, Text, View, 
+  Image, SafeAreaView, 
+  TouchableOpacity, 
+  Alert, Button, Platform, Dimensions,
+  StatusBar
+ } 
+  from "react-native";
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks'; 
 
 export default function App() {
+  const { landscape } = useDeviceOrientation();
 
   return (
-    <SafeAreaView style={[containerStyle, styles.container ]}>
-     <Button 
-     title="click me" onPress={() => 
-     Alert.alert("Custom prompt", "Is it easy to code in React Native?", [{text: 'yes', onPress: () => console.log("pressed")}])}/>
+    <SafeAreaView style = {styles.container}>
+      <View style={{
+        backgroundColor: 'dodgerBlue',
+        width: 150,
+        height: landscape ? "100%" : "30%",
+      }} />
     </SafeAreaView>
   );
 }
 
-const containerStyle = { backgroundColor: "red" };
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "yellow",
     alignItems: "center",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
 });
