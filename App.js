83ee117ae0da29670042863695f1f9d462e42527
arsalon <arsalonamini2020@gmail.com>
2020-10-27 14:43:46 +0800

    Alert standard function in React Native

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, Alert, Button } from "react-native";
 
 export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native! </Text>
-      <TouchableOpacity onPress={()=> console.log('image tapped')}> 
-      <Image 
-      fadeDuration={1000}
-      source={{ 
-        width: 200,
-        height: 300,
-        uri: "https://picsum.photos/200/300"}}/>
-      </TouchableOpacity>
-      
-      <StatusBar style="auto" />
+     <Button 
+     title="click me" onPress={() => alert('button tapped')}/>
     </SafeAreaView>
   );
 }
