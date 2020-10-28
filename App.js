98a25a896c6a6e303216b4bac434c88d146b82f2
arsalon <arsalonamini2020@gmail.com>
2020-10-28 16:06:09 +0800

    Screen: WelcomeScreen on App with logo and placeholders for login / register buttons

--- App.js
+++ App.js
@@ -1,50 +1,10 @@
 import React from "react";
-import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: '#fff',  flex: 1,
-     flexDirection: "row", //primary axis - horizontal
-     justifyContent: "center", //primary axis (center)
-     alignContent:"center", //secondary axis - vertical 
-     flexWrap: "wrap"
-     }}
-     > 
-
-    <View 
-      style={{
-        backgroundColor: 'dodgerblue', 
-        width: 100, 
-        height: 100, 
-      }}
-        />
-     <View 
-      style={{
-        backgroundColor: 'gold', 
-        width: 100, 
-        height: 100,
-       }}
-        />
-          <View 
-      style={{
-        backgroundColor: 'tomato', 
-        width: 100, 
-        height: 100, }}
-        />
-            <View 
-      style={{
-        backgroundColor: 'green', 
-        width: 100, 
-        height: 100, }}
-        />
-              <View 
-      style={{
-        backgroundColor: 'blue', 
-        width: 100, 
-        height: 100, }}
-        />
-    </View>
-  );
+   <WelcomeScreen />
+  )
 }
 
 
