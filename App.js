ffd3dd625a18ff0b9a2d1a9733866b509481bc09
arsalon <arsalonamini2020@gmail.com>
2020-10-28 12:23:47 +0800

    aligning items along primary axis using justifyContent property of flex

--- App.js
+++ App.js
@@ -5,16 +5,29 @@ import { View } from "react-native";
 export default function App() {
   return (
     <View style={{ backgroundColor: '#fff',  flex: 1,
-     flexDirection: "row", //horizontal
-     justifyContent: "center", // main 
-     alignItems: "baseline"
+     flexDirection: "column", //horizontal
+     justifyContent: "space-evenly" // main 
      }}
      > 
 
-    <View style={{backgroundColor: 'dodgerblue', width: 100, height: 300, }}/>
-    <View style={{backgroundColor: 'gold', width: 100, height: 300,  }}/>
-    <View style={{backgroundColor: 'tomato', width: 100, height: 200, }}/>
-
+    <View 
+      style={{
+        backgroundColor: 'dodgerblue', 
+        width: 100, 
+        height: 100, }}
+        />
+     <View 
+      style={{
+        backgroundColor: 'gold', 
+        width: 100, 
+        height: 100, }}
+        />
+          <View 
+      style={{
+        backgroundColor: 'tomato', 
+        width: 100, 
+        height: 100, }}
+        />
     </View>
   );
 }
