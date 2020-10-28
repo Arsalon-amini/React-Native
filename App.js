82260f3fa8da736ef2257279dc850ae6c7eb3aad
arsalon <arsalonamini2020@gmail.com>
2020-10-28 21:49:11 +0800

    Styles: boarders and making a circle with borderRadius

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
 import React from "react";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import { View } from 'react-native';
 
 export default function App() {
   return (
-   <WelcomeScreen />
+   <View 
+   style={{
+     flex: 1, //default "column" y-axis align
+     justifyContent: "center", //align items on main axis in center (y-axis)
+     alignItems: "center" //align items along x-axis (secondary)
+   }}
+   > 
+   <View
+   style={{
+     backgroundColor: "dodgerblue",
+     width: 100,
+     height: 100,
+     borderWidth: 10,
+     borderColor: "royalblue",
+     borderRadius: 50 //round edges (1/2 width makes circle)
+   }}
+   ></View>
+
+   </View>
   )
 }
 
