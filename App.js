2eb5dfe777993acc4b10fccc1f019373ffbc57cc
arsalon <arsalonamini2020@gmail.com>
2020-10-29 12:37:14 +0800

    Styles: alternative approach to styling components with separate folder (HINT: choose one approach and stick with it, don't change throughout the app

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { View } from 'react-native';
-import AppText from "./app/components/AppText";
 
-
+import AppText from './app/components/AppText'; 
 
 export default function App() {
   return (
    <View
    style={{
-     flex: 1,
+     flex:1,
      justifyContent: "center",
      alignItems: "center"
    }}
-   >  
-   <AppText> I love react native </AppText>
+   >
+     <AppText> I love React Native! </AppText>
+   
    </View>
   )
 }
-
 
 
