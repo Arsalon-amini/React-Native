d5ef9ed334e53251c15038f4e942edcfc9cb42e0
arsalon <arsalonamini2020@gmail.com>
2020-10-29 17:32:33 +0800

    New Screen: ListingDetailsScreen component with styling

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import React from "react";
 import { View } from 'react-native';
 
-import Card from './app/components/Card'
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
   return(
-      <View style={{
-        backgroundColor: '#f8f4f4', //grey background 
-        padding: 20,
-        paddingTop: 100
-      }}>
-        <Card 
-        title="Red jacket for Sale"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-        />
-      </View>
+      <ListingDetailsScreen />
   )
 }
 
---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+import AppText from '../components/AppText'; 
+import colors from '../config/colors'; 
+
+const ListingDetailsScreen = (props) => {
+    return ( 
+    <View style={styles.detailsContainer}>
+        <Image style={styles.image} source={require('../assets/jacket.jpg')}/>
+        <AppText style={styles.title}> Red Jacket For Sale </AppText>
+        <AppText style={styles.price}> $100 </AppText>
+    </View> );
+}
+
+const styles = StyleSheet.create({
+    detailsContainer: {
+        padding: 20,
+    },
+    image: {
+        width: '100%',
+        height: 300
+    },
+    price: {
+        color: colors.secondary,
+        fontWeight: "bold",
+        fontSize: 20,
+        marginVertical: 10
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "500"
+    }
+})
+ 
+export default ListingDetailsScreen;
