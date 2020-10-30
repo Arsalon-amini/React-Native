9564627ee35316bea82505bdbbad11003b0e7a77
arsalon <arsalonamini2020@gmail.com>
2020-10-30 23:14:00 +0800

    Re-usable component: re-usable Icon component

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ListItem from "./app/components/ListItem";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 import MessagesScreen from "./app/screens/MessagesScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import Icon from './app/components/Icon';
+import Screen from './app/components/Screen'; 
 
 
 export default function App() {
   return (
-    <MessagesScreen />
+    <Screen> 
+      <Icon 
+      name="email"
+      size={50}
+      backgroundColor= "red"
+      iconColor= "white"
+      />
+    </Screen>
   )
 }
 
