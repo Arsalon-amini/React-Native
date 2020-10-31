9a878076870a759d5782016fa9f55fe8aaf118ff
arsalon <arsalonamini2020@gmail.com>
2020-10-31 14:11:08 +0800

    ListItem Component Extension: adding support for conditionally rendering title/subtitle/image and support for handling icon rendering

--- App.js
+++ App.js
@@ -11,11 +11,9 @@ import Screen from './app/components/Screen';
 export default function App() {
   return (
     <Screen> 
-      <Icon 
-      name="email"
-      size={50}
-      backgroundColor= "red"
-      iconColor= "white"
+      <ListItem 
+      title="T1" 
+      ImageComponent={<Icon name="email"/>}
       />
     </Screen>
   )
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -5,17 +5,16 @@ import AppText from './AppText';
 import colors from '../config/colors'; 
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 
-const ListItem = ({title, subTitle, image, onPress, renderRightActions }) => {
+const ListItem = ({title, subTitle, image, ImageComponent, onPress, renderRightActions }) => {
     return ( 
         <Swipeable renderRightActions={renderRightActions}>
-        <TouchableHighlight 
-        underlayColor={colors.light} 
-        onPress={ onPress }>
+        <TouchableHighlight underlayColor={colors.light} onPress={ onPress }>
         <View style={styles.container}>
-        <Image source={image} style={styles.image} />
-        <View>
+            {ImageComponent}
+            {image && <Image source={image} style={styles.image} />}
+        <View style={styles.detailsContainer}>
             <AppText style={styles.title} >{title}</AppText>
-            <AppText style={styles.subTitle}>{subTitle}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
         </View>
         </View>
         </TouchableHighlight>
@@ -27,11 +26,14 @@ const styles = StyleSheet.create({
         flexDirection: "row", //layout components horizontally 
         padding: 15
     },
+    detailsContainer: {
+        marginLeft: 10,
+        justifyContent: "center"
+    },
     image: {
         width: 70,
         height: 70,
         borderRadius: 35,
-        marginRight: 10
     },
     subTitle: {
         color: colors.medium
