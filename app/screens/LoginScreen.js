f3403d4585bcd45ead0da541db0cd071cf1d9803
arsalon <arsalonamini2020@gmail.com>
2020-11-02 17:03:49 +0800

    Refactor Login Screen - extract AppFormField component to encapsulate AppTextInput (onBlur, onChangeText) and ErrorMessage into a re-usable FormField component

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -3,11 +3,9 @@ import { StyleSheet, Image } from 'react-native';
 import { Formik } from 'formik'; 
 import * as Yup from 'yup'; 
 
-import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
-import AppText from '../components/AppText'; 
-import ErrorMessage from '../components/ErrorMessage'; 
 import Screen from '../components/Screen'; 
+import AppFormField from '../components/AppFormField';
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"),
@@ -25,28 +23,24 @@ function LoginScreen(props) {
             >
                 { ({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
                     <> 
-                        <AppTextInput 
+                        <AppFormField
                         autoCapitalize="none"
                         autoCorrect={false}
                         icon="email"
                         keyboardType='email-address'
-                        onBlur={() => setFieldTouched("email")}
-                        onChangeText={handleChange('email')}
+                        name='email'
                         placeholder="Email"
                         textContentTye="emailAddress" 
                         />
-                        <ErrorMessage error={errors.email} visible={touched.email}/>
-                        <AppTextInput 
+                        <AppFormField
                         autoCapitalize="none"
                         autoCorrect={false}
                         icon="lock"
-                        onBlur={() => setFieldTouched("password")}
-                        onChangeText={handleChange('password')}
+                        name="password"
                         placeholder="Password"
-                        secureTextEntry 
-                        textContentType="password" 
+                        secureTextEntry
+                        textContentType="password"
                         />
-                        <ErrorMessage error={errors.password} visible={touched.password}/>
                         <AppButton 
                         title="Login"
                         onPress={handleSubmit} 
