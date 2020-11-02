96dd54a28b16241887dcfb34a37ff0a9854c2416
arsalon <arsalonamini2020@gmail.com>
2020-11-02 17:15:14 +0800

    Refactor LoginScreen - extract SubmitButton into a re-usable component encapsulating AppButton (title, onPress)

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -3,9 +3,9 @@ import { StyleSheet, Image } from 'react-native';
 import { Formik } from 'formik'; 
 import * as Yup from 'yup'; 
 
-import AppButton from '../components/AppButton';
 import Screen from '../components/Screen'; 
 import AppFormField from '../components/AppFormField';
+import SubmitButton from '../components/SubmitButton';
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"),
@@ -21,7 +21,7 @@ function LoginScreen(props) {
             onSubmit={values => console.log (values)} 
             validationSchema={validationSchema}
             >
-                { ({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
+                { () => (
                     <> 
                         <AppFormField
                         autoCapitalize="none"
@@ -41,10 +41,7 @@ function LoginScreen(props) {
                         secureTextEntry
                         textContentType="password"
                         />
-                        <AppButton 
-                        title="Login"
-                        onPress={handleSubmit} 
-                        />
+                        <SubmitButton title="login"/>
                     </>
                 ) }
             </Formik>  
