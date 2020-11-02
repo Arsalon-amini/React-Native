a143ec3473a90ffcd3b857882593b01febf335f7
arsalon <arsalonamini2020@gmail.com>
2020-11-02 15:49:16 +0800

    Refactor LoginScreen - added validation from Yup  using Formik for handling state tracking and error validation

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { StyleSheet, Image } from 'react-native';
 import { Formik } from 'formik'; 
+import * as Yup from 'yup'; 
 
 import Screen from '../components/Screen'; 
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
+import AppText from '../components/AppText'; 
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label("Email"),
+    password: Yup.string().required().min(4).label("Password")
+});
 
 function LoginScreen(props) {
     return (
@@ -13,8 +20,9 @@ function LoginScreen(props) {
             <Formik
             initialValues={{ email: '', password: '' }}
             onSubmit={values => console.log (values)} 
+            validationSchema={validationSchema}
             >
-                { ({ handleChange, handleSubmit }) => (
+                { ({ handleChange, handleSubmit, errors }) => (
                     <> 
                         <AppTextInput 
                         autoCapitalize="none"
@@ -23,8 +31,9 @@ function LoginScreen(props) {
                         keyboardType='email-address'
                         onChangeText={handleChange('email')}
                         placeholder="email"
-                        textContentTye="emailAddress" //iOS autofill from key-chain
+                        textContentTye="emailAddress" 
                         />
+                        <AppText style={{color: 'red'}}>{errors.email}</AppText>
                         <AppTextInput 
                         autoCapitalize="none"
                         autoCorrect={false}
@@ -32,18 +41,16 @@ function LoginScreen(props) {
                         onChangeText={handleChange('password')}
                         placeholder="Password"
                         secureTextEntry 
-                        textContentType="password" //iOS autofill from key-chain
+                        textContentType="password" 
                         />
+                        <AppText style={{color: 'red'}}>{errors.password}</AppText>
                         <AppButton 
                         title="Login"
-                        onPress={handleSubmit} //after submit, moves to onSubmit method
+                        onPress={handleSubmit} 
                         />
                     </>
                 ) }
-
-            </Formik>
-            
-            
+            </Formik>  
         </Screen>
     );
 }
