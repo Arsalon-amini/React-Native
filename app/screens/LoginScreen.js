fe13bb1014672a0a39931bd20cf4168ecc9bfa8c
arsalon <arsalonamini2020@gmail.com>
2020-11-02 17:41:04 +0800

    Refactor Forms into separate file - extract into re-usable forms files, cleaned up code in LoginScreen - Final refactor

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { StyleSheet, Image } from 'react-native';
-import { Formik } from 'formik'; 
 import * as Yup from 'yup'; 
 
 import Screen from '../components/Screen'; 
-import AppFormField from '../components/AppFormField';
-import SubmitButton from '../components/SubmitButton';
+import { AppForm, AppFormField, SubmitButton } from '../components/forms'; 
+
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"),
@@ -16,35 +15,31 @@ function LoginScreen(props) {
     return (
         <Screen style={styles.container}>
             <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-            <Formik
+            <AppForm
             initialValues={{ email: '', password: '' }}
             onSubmit={values => console.log (values)} 
             validationSchema={validationSchema}
             >
-                { () => (
-                    <> 
-                        <AppFormField
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        icon="email"
-                        keyboardType='email-address'
-                        name='email'
-                        placeholder="Email"
-                        textContentTye="emailAddress" 
-                        />
-                        <AppFormField
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        icon="lock"
-                        name="password"
-                        placeholder="Password"
-                        secureTextEntry
-                        textContentType="password"
-                        />
-                        <SubmitButton title="login"/>
-                    </>
-                ) }
-            </Formik>  
+            <AppFormField
+                autoCapitalize="none"
+                autoCorrect={false}
+                icon="email"
+                keyboardType='email-address'
+                name='email'
+                placeholder="Email"
+                textContentTye="emailAddress" 
+            />
+            <AppFormField
+                autoCapitalize="none"
+                autoCorrect={false}
+                icon="lock"
+                name="password"
+                placeholder="Password"
+                secureTextEntry
+                textContentType="password"
+                />
+            <SubmitButton title="login"/>
+            </AppForm>  
         </Screen>
     );
 }
