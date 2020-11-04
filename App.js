d04c43c9307769737c2d97cb34587bae42c6faaa
arsalon <arsalonamini2020@gmail.com>
2020-11-04 16:05:05 +0800

    Card component update - limited AppText component to display 1 line of text to fix too much text displayed issue

--- App.js
+++ App.js
@@ -4,15 +4,17 @@ import ListItem from './app/components/lists/ListItem'
 
 import MessagesScreen from './app/screens/MessagesScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import Card from './app/components/Card'; 
 import Screen from './app/components/Screen';
 
 export default function App() {
   return (
     <Screen>
-      <ListItem 
-      image={require("./app/assets/headshot.jpg")}
+      <Card
+      image={require("./app/assets/couch.jpg")}
       title="Arsalon A."
-      subTitle="8 listings
+      subTitle="
+      8 listings
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris iaculis, orci vitae ultrices mattis, sapien nisl tempus metus, ac vestibulum justo velit eu ipsum. Vivamus vel lacus vitae tortor ornare venenatis. Phasellus aliquam ex blandit, placerat massa quis, aliquam magna. Cras dui nulla, tincidunt quis pulvinar in, iaculis non arcu. Integer non est leo. Praesent metus dui, convallis non elementum aliquam, ornare quis velit. Nulla facilisi. Aliquam in aliquet purus. Nunc viverra vestibulum facilisis. Donec eget molestie sapien, ut scelerisque felis. Suspendisse potenti. Fusce in tristique tortor.
 
 Donec congue vehicula lacus, vel porttitor purus tempor id. Morbi rutrum eu sapien eget luctus. Aenean ultrices condimentum mauris non venenatis. Quisque a nisl eget odio egestas tempor ac in mi. Pellentesque suscipit molestie varius. Aenean ipsum risus, efficitur in tortor suscipit, venenatis bibendum eros. Donec mattis dictum leo sed vestibulum. Vivamus pulvinar eget dolor vel eleifend. Nullam tincidunt, augue non rhoncus pellentesque, justo sapien dignissim arcu, in pretium est urna id mauris. Donec convallis ipsum ligula, et feugiat augue cursus non. Proin convallis enim erat, sed sagittis sapien pulvinar ac. In hac habitasse platea dictumst. Curabitur faucibus dolor et eros pellentesque, quis porttitor diam accumsan. Sed luctus felis vitae mauris auctor, sit amet iaculis augue sodales. Suspendisse lacus ex, tincidunt id imperdiet vel, pretium non leo. In turpis risus, rutrum in est a, semper ornare sem.
@@ -22,7 +24,6 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non venenatis ju
 Integer vitae viverra augue, non suscipit purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam dui sapien, pulvinar ut lectus et, pharetra interdum orci. Maecenas venenatis fermentum leo vel vulputate. Nunc ut elit risus. Donec sed congue tellus, sed finibus risus. Quisque vitae eros nec dolor dignissim tristique vitae vitae nibh. Suspendisse potenti. Aenean lobortis vulputate bibendum.
 
 Curabitur vel ullamcorper nisi, vitae vehicula enim. Sed interdum tincidunt viverra. Morbi venenatis, ante eget rutrum convallis, odio lacus tincidunt leo, id accumsan nunc felis vel erat. Suspendisse in ultrices erat. Nulla mattis vel neque at elementum. Sed iaculis luctus felis, quis euismod tellus blandit eget. In hac habitasse platea dictumst. Duis pulvinar erat sed tempus venenatis. Nam finibus vulputate erat, non efficitur ligula elementum in. Nulla facilisi.
-      
       "
     />
     </Screen>
