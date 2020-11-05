3b02b30f9c424b835021aefa9662497f53eeb49a
arsalon <arsalonamini2020@gmail.com>
2020-11-05 16:28:38 +0800

    ImageInput Component - display a chosen image from user's library in a view, with styling and an Icon if no image selected

--- App.js
+++ App.js
@@ -1,38 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import * as ImagePicker from 'expo-image-picker'; 
-import * as Permissions from 'expo-permissions'; 
 
 import Screen from './app/components/Screen';
 import { Button, Image } from 'react-native';
+import ImageInput from './app/components/ImageInput'
 
 
 export default function App() {
-  const [imageUri, setImageUri] = useState()
+  const [imageUri, setImageUri] = useState(); 
 
-
-  const requestPermission = async() => {
-    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
-    if(!granted)  
-      alert("You need to enable permissions to access library")
+  const requestPermissions = async() => {
+      const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+      if(!granted)
+          alert("You need to enable permissions to access photo library") 
   }
   useEffect(() => {
-    requestPermission(); //cannot call async fn directly
+      requestPermissions(); 
   }, [])
 
-  const selectImage= async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync(); 
-      if(!result.cancelled)
-        setImageUri(result.uri); 
-    } catch (error) {
-      console.log('Error reading and Image'); 
-    }
+  const selectImage = async () => {
+      try {
+          const result = await ImagePicker.launchImageLibraryAsync(); 
+          if(!result.cancelled)
+              setImageUri(result.uri);  
+      } catch (error) {
+          console.log('Error reading an Image'); 
+      }
   }
-
   return (
     <Screen>
-      <Button title="Select Image" onPress={selectImage}></Button>
-      <Image source={{uri: imageUri}} style={{width: 200, height: 200 }}/>
+      <Button title="select image" onPress={selectImage} />
+      <ImageInput imageUri={imageUri} />
     </Screen>
   )
 }
