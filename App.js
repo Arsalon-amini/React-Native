8ff2c90557dd037cd482c4e7f65309329960bb29
arsalon <arsalonamini2020@gmail.com>
2020-11-05 15:51:02 +0800

    Function - display image picked from library on screen using state hook and ImagePicker from expo

--- App.js
+++ App.js
@@ -1,23 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as ImagePicker from 'expo-image-picker'; 
 import * as Permissions from 'expo-permissions'; 
 
 import Screen from './app/components/Screen';
+import { Button, Image } from 'react-native';
 
 
 export default function App() {
+  const [imageUri, setImageUri] = useState()
+
+
   const requestPermission = async() => {
-    const result = Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION)
-    if(!result.granted)
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if(!granted)  
       alert("You need to enable permissions to access library")
   }
-
   useEffect(() => {
     requestPermission(); //cannot call async fn directly
   }, [])
 
+  const selectImage= async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync(); 
+      if(!result.cancelled)
+        setImageUri(result.uri); 
+    } catch (error) {
+      console.log('Error reading and Image'); 
+    }
+  }
+
   return (
-      <Screen></Screen>
+    <Screen>
+      <Button title="Select Image" onPress={selectImage}></Button>
+      <Image source={{uri: imageUri}} style={{width: 200, height: 200 }}/>
+    </Screen>
   )
 }
 
