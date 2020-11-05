de5235f5e09ffab8894a4d1156ff13993c417516
arsalon <arsalonamini2020@gmail.com>
2020-11-05 15:26:32 +0800

    Expo ImagePicker for accessing native feature of camera

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ListingEditScreen from './app/screens/ListingEditScreen';
-import ListItem from './app/components/lists/ListItem'
+import React, { useEffect } from 'react';
+import * as ImagePicker from 'expo-image-picker'; 
 
-import MessagesScreen from './app/screens/MessagesScreen';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import Card from './app/components/Card'; 
 import Screen from './app/components/Screen';
-import AccountScreen from './app/screens/AccountScreen';
-import RegisterScreen from './app/screens/RegisterScreen';
-import LoginScreen from './app/screens/LoginScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
+
 
 export default function App() {
+  const requestPermission = async() => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if(!granted)
+      alert("You need to enable permissions to access library")
+  }
+
+  useEffect(() => {
+    requestPermission(); //cannot call async fn directly
+  }, [])
+
   return (
-      <WelcomeScreen />
+      <Screen> </Screen>
   )
 }
 
