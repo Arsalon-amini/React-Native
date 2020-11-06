189f72a2e68fe8ac65c1995489af688ef261a9eb
arsalon <arsalonamini2020@gmail.com>
2020-11-06 23:23:07 +0800

    Stack Navigator Demo - using createStackNavigator cmponent

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import Picker from './app/components/Picker';
+import { Text } from 'react-native'; 
+import { createStackNavigator } from '@react-navigation/stack'; 
+import { NavigationContainer } from '@react-native/native'; 
 
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import Screen from './app/components/Screen'; 
 
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+        <Text>Tweet Details </Text>
+      </Screen>
+)
+
+const Stack = createStackNavigator();  //has navigator/screen (routes) properties act like components 
+const StackNavigator = () => (
+  <Stack.Navigator initialRouteName="Tweets">
+    <Stack.Screen name="Tweets" component={Tweets}/> 
+    <Stack.Screen name="TweetDetails" component={TweetDetails}/> 
+  </Stack.Navigator>
+)
 
 export default function App() {
   return (
-      <ListingEditScreen />
+    <NavigationContainer>
+      <StackNavigator /> 
+    </NavigationContainer>
+      
   )
 }
 
