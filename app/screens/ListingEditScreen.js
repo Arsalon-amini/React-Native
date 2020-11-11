06e5ef3f907b5d4f2b34597eae1e71b9bf37d96d
arsalon <arsalonamini2020@gmail.com>
2020-11-11 17:13:54 +0800

    UploadScreen update - added conditional rendering to display LottieView animation upon upload completion ; in ListingEditScreen handled the onDone event raised by LottieView to hide animation

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -24,15 +24,60 @@ const validationSchema = Yup.object().shape({
 });
 
 const categories = [
-  { label: "Furniture", value: 1, backgroundColor: 'green', icon: 'apps'},
-  { label: "Cars", value: 2, backgroundColor: 'green', icon: 'car'},
-  { label: "Cameras", value: 3, backgroundColor: 'blue', icon: 'camera' },
-  { label: "Games", value: 4, backgroundColor: 'orange', icon: 'gamepad'},
-  { label: "Clothing", value: 5, backgroundColor: 'blue', icon: 'tshirt-crew'},
-  { label: "Sports", value: 6, backgroundColor: 'red', icon: 'run' },
-  { label: "Music & Movies", value: 7, backgroundColor: 'red', icon: 'bookmark-music'},
-  { label: "Books", value: 8, backgroundColor: 'green', icon: 'book'},
-  { label: "Other", value: 9, backgroundColor: 'grey', icon: 'lock-question' },
+  {
+    backgroundColor: "#fc5c65",
+    icon: "floor-lamp",
+    label: "Furniture",
+    value: 1,
+  },
+  {
+    backgroundColor: "#fd9644",
+    icon: "car",
+    label: "Cars",
+    value: 2,
+  },
+  {
+    backgroundColor: "#fed330",
+    icon: "camera",
+    label: "Cameras",
+    value: 3,
+  },
+  {
+    backgroundColor: "#26de81",
+    icon: "cards",
+    label: "Games",
+    value: 4,
+  },
+  {
+    backgroundColor: "#2bcbba",
+    icon: "shoe-heel",
+    label: "Clothing",
+    value: 5,
+  },
+  {
+    backgroundColor: "#45aaf2",
+    icon: "basketball",
+    label: "Sports",
+    value: 6,
+  },
+  {
+    backgroundColor: "#4b7bec",
+    icon: "headphones",
+    label: "Movies & Music",
+    value: 7,
+  },
+  {
+    backgroundColor: "#a55eea",
+    icon: "book-open-variant",
+    label: "Books",
+    value: 8,
+  },
+  {
+    backgroundColor: "#778ca3",
+    icon: "application",
+    label: "Other",
+    value: 9,
+  },
 ];
 
 function ListingEditScreen() {
@@ -45,16 +90,21 @@ function ListingEditScreen() {
     const result = await listingsApi.addListing(
         {...listing, location }, 
         progress => setProgress(progress)
-      ); 
-      setUploadVisible(false); 
+      );  
 
-    if(!result.ok) return alert('Could not save the listing. '); 
-    alert ('Success'); 
+    if(!result.ok) {
+      setUploadVisible(false);
+      return alert('Could not save the listing. ');
+    }
   }
 
   return (
     <Screen style={styles.container}>
-      <UploadScreen progress={progress} visible={uploadVisible}/>
+      <UploadScreen 
+      onDone={() => setUploadVisible(false)} //handleEvent (parent)
+      progress={progress} 
+      visible={uploadVisible}
+      />
       <Form
         initialValues={{
           title: "",
--- app/screens/UploadScreen.js
+++ app/screens/UploadScreen.js
@@ -1,29 +1,43 @@
-import React from 'react';
-import { StyleSheet, View, Modal } from 'react-native';
-import * as Progress from 'react-native-progress'; 
-import { color } from 'react-native-reanimated';
+import React from "react";
+import { View, StyleSheet, Modal } from "react-native";
+import * as Progress from "react-native-progress";
+import LottieView from "lottie-react-native";
 
-function UploadScreen({ progress = 0, visible = false }) {
-    return (
-        <Modal visible={visible}>
-            <View style={styles.container}>
-                <Progress.Bar 
-                    color={color.primary} 
-                    progress={progress} 
-                    width={200}
-                />
-            </View>
-        </Modal>
- );
+import colors from "../config/colors";
+
+function UploadScreen({ onDone, progress = 0, visible = false }) {
+  return (
+    <Modal visible={visible}>
+      <View style={styles.container}>
+        {progress < 1 ? (
+          <Progress.Bar
+            color={colors.primary}
+            progress={progress}
+            width={200}
+          />
+        ) : (
+          <LottieView
+            autoPlay
+            loop={false}
+            onAnimationFinish={onDone}
+            source={require("../assets/animations/done.json")}
+            style={styles.animation}
+          />
+        )}
+      </View>
+    </Modal>
+  );
 }
 
 const styles = StyleSheet.create({
-   container: {
-       alignItems: 'center',
-       justifyContent: 'center',
-       flex: 1
-   }
-
-})
+  animation: {
+    width: 150,
+  },
+  container: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+  },
+});
 
 export default UploadScreen;
