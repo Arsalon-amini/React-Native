2f606c90e0398734cbbca5a8f81d1d8d2497aeca
arsalon <arsalonamini2020@gmail.com>
2020-11-12 14:10:06 +0800

    Class Components Example - ComponentDidMount/ComponentWillUnMount use of the NetInfo.addEventListener( ) method for detecting network changes

--- App.js
+++ App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import NetInfo from '@react-native-community/netinfo';
 
 export default function App() {
-    NetInfo.fetch().then(netInfo => console.log(netInfo)); 
+
+  //call in ComponentDidMount
+    const unsubscribe = NetInfo.addEventListener(netInfo => console.log(netInfo)); //called everytime a change in network 
+
+  //call in Component will unMount 
+  unsubscribe(); 
   return (
     null
   )
