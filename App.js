a95ab2246830ae4973daebe44abd79775b8984bb
arsalon <arsalonamini2020@gmail.com>
2020-11-12 14:05:05 +0800

    NetInfo library - determining isConnected, isInternetReachable inorder to offer offline capabilities to user

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { Text, Button } from 'react-native'; 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; 
-import { NavigationContainer, useNavigation } from '@react-navigation/native'; 
-import { createStackNavigator } from '@react-navigation/stack'; 
-
-import Screen from './app/components/Screen'; 
-import navigationTheme from './app/navigation/navigationTheme';
-import AppNavigator from './app/navigation/AppNavigator';
-import AuthNavigator from './app/navigation/AuthNavigator';
-
+import NetInfo from '@react-native-community/netinfo';
 
 export default function App() {
+    NetInfo.fetch().then(netInfo => console.log(netInfo)); 
   return (
-    <NavigationContainer theme={navigationTheme} >
-      <AppNavigator/> 
-    </NavigationContainer>
+    null
   )
 }
 
