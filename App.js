9901e521ef41f34e5815f4e1c0472b3b5cf1447e
arsalon <arsalonamini2020@gmail.com>
2020-11-13 19:51:46 +0800

    auth.js - abstraction over client (abstracted apiSauce) for sending email / password to server, use in LoginScreen with Formik onSubmit to handleSubmit by calling auth.login (post request to server with email/password)

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import AppNavigator from './app/navigation/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import navigationTheme from './app/navigation/navigationTheme';
 import OfflineNotice from './app/components/OfflineNotice';
+import AuthNavigator from './app/navigation/AuthNavigator';
 
 export default function App() {
   return (
     <>
       <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
-        <AppNavigator />
+        <AuthNavigator />
       </NavigationContainer>
     </>
   )
