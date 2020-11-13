5442a966475f61e44df5242546b920c6476b2244
arsalon <arsalonamini2020@gmail.com>
2020-11-13 18:43:21 +0800

    OfflineNotice component - render a message to the user if mobile device loses an internet connection

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import AppNavigator from './app/navigation/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import navigationTheme from './app/navigation/navigationTheme';
+import OfflineNotice from './app/components/OfflineNotice';
 
 export default function App() {
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer theme={navigationTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   )
 }
 
