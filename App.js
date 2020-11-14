0399fd0cb51273ca8b31117bad59dd0fc8cff8bf
arsalon <arsalonamini2020@gmail.com>
2020-11-14 20:42:54 +0800

    AppLoading component - to display a custom splash screen that restores authtoken on startup of application

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import AppNavigator from './app/navigation/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import jwtDecode from 'jwt-decode'; 
+import { AppLoading } from 'expo'; 
 
 import navigationTheme from './app/navigation/navigationTheme';
 import OfflineNotice from './app/components/OfflineNotice';
@@ -11,15 +12,19 @@ import authStorage from './app/auth/storage';
 
 export default function App() {
   const [user, setUser] = useState(); 
+  const [isReady, setIsReady] = useState(false); 
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
     if(!token) return; 
     setUser(jwtDecode(token)); 
   }
-  useEffect(() => {
-    restoreToken(); //setUser after restart
-  },[])
+
+  if(!isReady) return (
+    <AppLoading 
+        startAsync={restoreToken} //when app loads startAsync fn called
+        onFinish={()=> setIsReady(true)} //called when startAsync fn execution finished
+    /> )
 
   return (
     <AuthContext.Provider value={{user, setUser}}>
