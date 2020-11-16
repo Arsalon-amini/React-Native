d01c9e897896e09e32cc6748bfb0799184087d93
arsalon <arsalonamini2020@gmail.com>
2020-11-16 17:31:38 +0800

    Local Notifications Demo - displaying a local notification using Notifications API presentNotificationsAsync from expo-notifications

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { Button } from 'react-native';
 import { AppLoading } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
+import * as Notifications from 'expo-notifications';
 
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
@@ -8,30 +10,22 @@ import OfflineNotice from './app/components/OfflineNotice';
 import AuthNavigator from './app/navigation/AuthNavigator';
 import AuthContext from './app/auth/context';
 import authStorage from './app/auth/storage';
-import { navigationRef } from './app/navigation/rootNavigation'; 
+import { navigationRef } from './app/navigation/rootNavigation';
+import Screen from './app/components/Screen';
 
 export default function App() {
-  const [user, setUser] = useState();
-  const [isReady, setIsReady] = useState(false);
 
-  const restoreUser = async () => {
-    const user = await authStorage.getUser();
-    if (user) setUser(user);
-  }
+  const showNotification = () => {
+    Notifications.presentNotificationAsync({
+      title: 'Congratulations',
+      body: 'Your order was successfully placed'
+    })
 
-  if (!isReady) return (
-    <AppLoading
-      startAsync={restoreUser} //when app loads startAsync fn called
-      onFinish={() => setIsReady(true)} //called when startAsync fn execution finished
-    />)
-
+  };
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
-      <OfflineNotice />
-      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
-        {user ? <AppNavigator /> : <AuthNavigator />}
-      </NavigationContainer>
-    </AuthContext.Provider>
+    <Screen>
+      <Button title="Tap me" onPress={showNotification} />
+    </Screen>
   )
 }
 
