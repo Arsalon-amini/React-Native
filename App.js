03ba1553fe3e4eebc6904c3b0865b7f74c3cf54d
arsalon <arsalonamini2020@gmail.com>
2020-11-16 18:04:32 +0800

    ListingDetailsScreen extension - added ContactSellerForm (abstraction over Form component, FormField (Formik) and sends a push notification to user in handleSubmit()

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import { Button } from 'react-native';
 import { AppLoading } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
-import * as Notifications from 'expo-notifications';
 
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
@@ -10,27 +8,30 @@ import OfflineNotice from './app/components/OfflineNotice';
 import AuthNavigator from './app/navigation/AuthNavigator';
 import AuthContext from './app/auth/context';
 import authStorage from './app/auth/storage';
-import { navigationRef } from './app/navigation/rootNavigation';
-import Screen from './app/components/Screen';
+import { navigationRef } from './app/navigation/rootNavigation'; 
 
 export default function App() {
+  const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
-  const showNotification = () => {
-    Notifications.scheduleNotificationAsync({
-      content: {
-        title: 'Dont forget your order!'
-      },
-      trigger: {
-        seconds: 4,
-      },
-    });
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
+  }
 
+  if (!isReady) return (
+    <AppLoading
+      startAsync={restoreUser} //when app loads startAsync fn called
+      onFinish={() => setIsReady(true)} //called when startAsync fn execution finished
+    />)
 
-  };
   return (
-    <Screen>
-      <Button title="Tap me" onPress={showNotification} />
-    </Screen>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <OfflineNotice />
+      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
+        {user ? <AppNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </AuthContext.Provider>
   )
 }
 
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,35 +1,42 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import { Image } from 'react-native-expo-image-cache'; 
 
 import Text from '../components/Text'; 
 import colors from '../config/colors'; 
 import ListItem from '../components/lists/ListItem';
+import ContactSellerForm from '../components/ContactSellerForm';
 
 const ListingDetailsScreen = ({ route }) => {
     const listing = route.params; 
 
     return ( 
-    <View>
-        <Image 
-        preview ={listing.images[0].uri} 
-        style={styles.image}
-        tint='light'
-        uri={listing.images[0].url}
-        />
-     <View style={styles.detailsContainer}>
-        <Text style={styles.title}> {listing.title} </Text>
-        <Text style={styles.price}> ${listing.price} </Text>
-            <View style={styles.userContainer}>
-                <ListItem 
-                    image={require("../assets/headshot.jpg")}
-                    title="Arsalon A."
-                    subTitle="8 listings"
-                />
-            </View> 
-        </View>
-    </View>
-    );
+        <KeyboardAvoidingView
+            behavior="position"
+            keyboardVerticalOffset={Platform.OS == "ios" ? 0 : 100 }
+        >
+            <Image 
+                preview ={listing.images[0].uri} 
+                style={styles.image}
+                tint='light'
+                uri={listing.images[0].url}
+            />
+            <View style={styles.detailsContainer}>
+                <Text style={styles.title}> {listing.title} </Text>
+                <Text style={styles.price}> ${listing.price} </Text>
+                    <View style={styles.userContainer}>
+                        <ListItem 
+                            image={require("../assets/headshot.jpg")}
+                            title="Arsalon A."
+                            subTitle="8 listings"
+                        />
+                    </View> 
+                </View>
+                <View>
+                    <ContactSellerForm listing={listing}/>
+                </View>
+        </KeyboardAvoidingView>
+        );
 }
 
 const styles = StyleSheet.create({
